3b07ed989c17d262b474d51d8ab625d6791d6eda
RVasiliy <vasiliy-mobile@yandex.ru>
2020-09-23 15:55:19 +0200

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {MarketingApiModule} from './api/marketing-api/marketing-api.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {MarketingApiModule} from './api/marketing-api/marketing-api.module';
   ],
   imports: [
     BrowserModule,
-    MarketingApiModule.config({baseUrl: 'http://localhost:4200'})
+    MarketingApiModule.config({baseUrl: 'http://localhost:4200'}),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
