e671db3aeac8251dddcff388267bef3aeb1981df
RVasiliy <vasiliy-mobile@yandex.ru>
2020-09-24 11:51:19 +0200

    Add total values in table

--- src/app/table-marketing-data/table-marketing-data.component.ts
+++ src/app/table-marketing-data/table-marketing-data.component.ts
@@ -3,7 +3,10 @@ import {MatTableDataSource} from '@angular/material/table';
 import {forkJoin, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
-import {MarketingApiDataProviderService} from '../common/marketing-data/providers/marketing-api-data-provider.service';
+import {
+  MarketingApiDataProvider,
+  MarketingApiDataProviderService
+} from '../common/marketing-data/providers/marketing-api-data-provider.service';
 
 interface TableData {
   [key: string]: any;
@@ -19,17 +22,18 @@ export class TableMarketingDataComponent implements OnInit {
   dataSource$: Observable<MatTableDataSource<TableData>>;
   configs$: Observable<{ key: string, title: string }[]>;
 
+  private dataProvider$: MarketingApiDataProvider;
+
   constructor(private marketingService: MarketingApiDataProviderService) {
+    this.dataProvider$ = this.marketingService.getDataProvider();
   }
 
   ngOnInit(): void {
-    const dataProvider = this.marketingService.getDataProvider();
-
-    this.displayedColumns$ = dataProvider.getColumns().pipe(map(columns => columns.map(column => column.key)));
+    this.displayedColumns$ = this.dataProvider$.getColumns().pipe(map(columns => columns.map(column => column.key)));
 
     this.dataSource$ = forkJoin([
-      dataProvider.getColumns(),
-      dataProvider.getData()
+      this.dataProvider$.getColumns(),
+      this.dataProvider$.getData()
     ]).pipe(
       map(([columns, rows]) => {
         return rows.map(values => {
@@ -45,11 +49,23 @@ export class TableMarketingDataComponent implements OnInit {
       map(rows => new MatTableDataSource(rows))
     );
 
-    this.configs$ = dataProvider.getColumns().pipe(map(columns => columns.map(column => {
-      return {
-        key: column.key,
-        title: column.title
-      };
-    })));
+    this.configs$ = this.dataProvider$.getColumns()
+      .pipe(
+        map(columns => columns.map(column => {
+            return {
+              key: column.key,
+              title: column.title
+            };
+          })
+        )
+      );
+  }
+
+  getTotalByColumn(key: string): Observable<any> {
+    return this.dataProvider$.getTotal().pipe(map(total => total[key]));
+  }
+
+  trackBy(index): number {
+    return index;
   }
 }
